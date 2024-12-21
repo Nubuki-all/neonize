8faf25ed4acfa865ba0de658b5b752df8c4e0b0a
Pah. <121189493+Nubuki-all@users.noreply.github.com>
2024-12-22 00:56:10 +0100

    Update client.py

--- neonize/aioze/client.py
+++ neonize/aioze/client.py
@@ -797,6 +797,8 @@ class NewAClient:
         quoted: Optional[neonize_proto.Message] = None,
         name: str = "",
         packname: str = "",
+        animated : bool = False,
+        bitrate : str = "",
         enforce_not_broken: bool = False,
     ) -> Message:
         """
@@ -832,7 +834,7 @@ class NewAClient:
         else:
             async with AFFmpeg(sticker) as ffmpeg:
                 animated = True
-                sticker = await ffmpeg.cv_to_webp(enforce_not_broken=enforce_not_broken)
+                sticker = await ffmpeg.cv_to_webp(animated, bitrate, enforce_not_broken)
                 io_save = BytesIO(sticker)
                 img = Image.open(io_save)
                 io_save.seek(0)
@@ -865,6 +867,8 @@ class NewAClient:
         quoted: Optional[neonize_proto.Message] = None,
         name: str = "",
         packname: str = "",
+        animated: bool = False,
+        bitrate: str = "",
         enforce_not_broken: bool = False,
     ) -> SendResponse:
         """
@@ -885,7 +889,7 @@ class NewAClient:
         """
         return await self.send_message(
             to,
-            await self.build_sticker_message(file, quoted, name, packname, enforce_not_broken),
+            await self.build_sticker_message(file, quoted, name, packname, animated, bitrate, enforce_not_broken),
         )
 
     async def build_video_message(
