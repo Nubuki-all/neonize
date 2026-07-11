778ef94e7e039cd6541aaf9895a9a8b05f1231cb
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2026-07-11 07:10:23 +0100

    update sticker.py

--- neonize/utils/sticker.py
+++ neonize/utils/sticker.py
@@ -56,7 +56,7 @@ def webpmux_is_installed():
 MAX_STICKER_SIZE = 512000
 WEBPMUX_IS_AVAILABLE = False
 if webpmux_is_installed():
-    MAX_STICKER_SIZE = 712000
+    MAX_STICKER_SIZE = 1000000
     WEBPMUX_IS_AVAILABLE = True
 
 
