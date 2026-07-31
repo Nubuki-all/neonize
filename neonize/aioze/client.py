7dc9cd56586c366f653a1b3a949ebfa4ea0742e8
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2026-07-31 14:57:52 +0100

    fix(quote): fix quoting some users messages

--- neonize/aioze/client.py
+++ neonize/aioze/client.py
@@ -601,7 +601,7 @@ class NewAClient:
         sender = message.Info.MessageSource.Sender
         if jid_is_lid(sender):
             senderalt = message.Info.MessageSource.SenderAlt
-            sender = senderalt if senderalt.ListFields() else sender
+            sender = senderalt if not senderalt.IsEmpty else sender
         return ContextInfo(
             stanzaID=message.Info.ID,
             participant=Jid2String(JIDToNonAD(sender)),
